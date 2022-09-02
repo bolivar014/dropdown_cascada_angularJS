650f83cc415c59e568789501afd39156a8683e83
Camilo Valdes <bolivar014@gmail.com>
2022-09-02 12:05:40 -0500

    integrando componentes

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+// Importamos interfaces
+import { CountryI, CityI } from 'src/app/models/model.interface'; 
+import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  providers: [DataService] // Inyección de propiedades
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataSvc: DataService) { }
 
   ngOnInit(): void {
+    console.log(this.dataSvc.getCountries());
+    console.log(this.dataSvc.getCities());
   }
 
 }
